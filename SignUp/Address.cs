2216678db30b0b80cf8cfb2fb217f5d372a1c552
K-andy13 <kmanderson@st.ug.edu.gh>
2024-07-06 15:24:29 +0000

    Address Book

---
+++ SignUp/Address.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SignUp
+{
+    public partial class Address : Form
+
+        
+    {public String Name{ set;get; }
+        public String email { set;get; }
+        public String phone {  set;get; }
+        public Address()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Name = textBox1.Text;
+            email = textBox2.Text;
+            phone = textBox3.Text;
+            MessageBox.Show("Saved");
+            textBox1.Clear();
+            textBox2.Clear();
+            textBox3.Clear();
+
+
+
+        }
+    }
+}
--- SignUp/Program.cs
+++ SignUp/Program.cs
@@ -16,7 +16,7 @@ namespace SignUp
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Signup());
+            Application.Run(new Address());
         }
     }
 }
